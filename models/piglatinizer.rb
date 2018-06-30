e63607ece6173a7ed48ea24e3ed4a5c71c46c033
Dmitri Mendis <2dmendis@gmail.com>
2018-06-30 03:53:00 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,7 @@ class PigLatinizer
   VOWELS = %w{a e i o u}
   
   def piglatinize(text)
-    
+    binding.pry
     if VOWELS.include?(text[0].downcase)
       piglatin = splitText << "way"
       final = piglatin.join("")
