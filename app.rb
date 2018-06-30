3c15a196e267ad9c0025fe046ff593b2412aa3be
Dmitri Mendis <2dmendis@gmail.com>
2018-06-30 02:57:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/piglatinize' do 
+    @piglatinized_text = PigLatinizer.new()
+    @pl_text = @piglatinized_text.piglatinize(params[:text_from_user])
+    erb :pig_latin
+  end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class PigLatinizer 
+ 
+  
+  def piglatinize(text)
+    
+    ending = "ay"
+    @new_sentence = []
+    @new_word = []
+    @new_word_array = []
+    
+    textArray = text.split(" ")
+    
+    textArray.map do |word|
+      @new_word.clear 
+      @new_word_array.clear
+      split_word = word.split(/([aeiou].*)/)
+      
+      split_word.map do |splitWord|
+        
+        @new_word_array.unshift(splitWord)
+      end 
+     
+      @new_word_array << ending
+      @new_word = @new_word_array.join
+      @new_sentence.push(@new_word)
+   
+    end 
+    @final_sentence = @new_sentence.join
+    @final_sentence
+    
+  end 
+end 
